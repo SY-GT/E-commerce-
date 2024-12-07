b7d23bc6218c33f6bbda8154c47287aaa4806b2a
SY-GT <money.power.sygt@gmail.com>
2024-12-07 10:06:12 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,5 +0,0 @@
-// Contact Form Submission Handler
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Thank you for reaching out to SYGT! We'll get back to you soon.");
-});
